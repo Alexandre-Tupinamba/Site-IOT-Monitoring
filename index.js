44da98068002c381cf052ca4bd3f71736a14487b
Alexandre Tupinamba <alexandre.icomp.ufam.edu.br>
2023-03-06 17:20:34 -0400

    V3

--- index.js
+++ index.js
@@ -1,118 +1,121 @@
 let nos = [];
-let conduct = [];
-let ph = [];
-let turbidez = [];
-let temp = [];
-
-
+let conduct;
+let ph;
+let turbidez;
+let temp;
+let dados;
+let teste = []
 // Executes when document is loaded
-document.addEventListener("DOMContentLoaded", (ev) => { 
-  DataObj.fetchData(); // look here
-  // Recent Orders Data
-  document.getElementById("recent-orders--table").appendChild(buildTableBody());
+document.addEventListener("DOMContentLoaded", (ev) => {
 
-  // Updates Data
-  document
-    .getElementsByClassName("recent-updates")
-    .item(0)
-    .appendChild(buildUpdatesList());
 
-  // Sales Analytics
-  const salesAnalytics = document.getElementById("analytics");
-  buildSalesAnalytics(salesAnalytics);
 });
 
 //Carrega dados do Firebase
 
 let DataObj = {
   get: "/Pai/.json",
-  fetchData : function () {
-      fetch(
-          "https://monitoramentoremotoico-default-rtdb.firebaseio.com"
-           + this.get
-      )
+  fetchData: function () {
+    fetch(
+      "https://monitoramentoremotoico-default-rtdb.firebaseio.com"
+      + this.get
+    )
       .then((Response) => Response.json())
       .then((data) => this.displayData(data));
   },
-  displayData: function(data) {
-    
-    console.log(data);
+  displayData: function (data) {
+    dados = data;
     for (const prop in data) {
       nos.push(prop);
     }
-    console.log(nos)
 
-      //const { name } = data;
-      //const { icon, description } = data.weather[0];
-      //const { temp, humidity } = data.Ph;
-      //const { speed } = data.wind;
-      //console.log(data.Ph)
-      //document.querySelector(".city").innerText = "Sensor de " + name;
-     // document.querySelector(".icon").src = 
-     //     "https://openweathermap.org/img/wn/" + icon + ".png";
-     // document.querySelector(".description").innerText = description;
-     // document.querySelector(".temp").innerText = temp + "°C";
-     // document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-     // document.querySelector(".wind").innerText = "Wind Speed: " + speed + "Km/h";
-     // document.querySelector(".weather").classList.remove("loading");
-      //document.body.style.backgroundImage = "url('https://source.unsplash.com/1700x1000/?" + name +"')"
+    console.log(nos)
+    // Recent Orders Data
+    document.getElementById("recent-orders--table").appendChild(buildTableBody());
+    document.getElementsByClassName("insights")[0].style.display = "grid"
+    // Updates Data
+    document
+      .getElementsByClassName("recent-updates")
+      .item(0)
+      .appendChild(buildUpdatesList());
+
+    // Sales Analytics
+    const salesAnalytics = document.getElementById("analytics");
+    buildSalesAnalytics(salesAnalytics);
+
+    //const { name } = data;
+    //const { icon, description } = data.weather[0];
+    //const { temp, humidity } = data.Ph;
+    //const { speed } = data.wind;
+    //console.log(data.Ph)
+    //document.querySelector(".city").innerText = "Sensor de " + name;
+    // document.querySelector(".icon").src = 
+    //     "https://openweathermap.org/img/wn/" + icon + ".png";
+    // document.querySelector(".description").innerText = description;
+    // document.querySelector(".temp").innerText = temp + "°C";
+    // document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    // document.querySelector(".wind").innerText = "Wind Speed: " + speed + "Km/h";
+    // document.querySelector(".weather").classList.remove("loading");
+    //document.body.style.backgroundImage = "url('https://source.unsplash.com/1700x1000/?" + name +"')"
   },
-  search: function(){
-      this.fetchData(document.querySelector(".search-bar").value);
+  search: function () {
+    this.fetchData(document.querySelector(".search-bar").value);
   },
 };
 
 
+DataObj.fetchData();
+
 // Document Builder
 const buildTableBody = () => {
-  const recentOrderData = nos;
+  const recentOrderData = RECENT_ORDER_DATA;
 
   const tbody = document.createElement("tbody");
 
   let bodyContent = "";
-  for (let no in recentOrderData){
-    ph = no["Ph"];
-    turbidez = no['Turbidez'];
-    conduct = no['Conduct'];
-    temp = no['Temp'];
-    for (const row of recentOrderData) {
-      bodyContent += `
-        <tr>
-          <td>${row.productName}</td>
-          <td>${row.productNumber}</td>
-          <td>${row.payment}</td>
-          <td class="${row.statusColor}">${row.status}</td>
-          <td class="primary">Details</td>
-        </tr>
-      `;
-    }
+  for (const row of recentOrderData) {
+    bodyContent += `
+      <tr>
+        <td>${row.productName}</td>
+        <td>${row.productNumber}</td>
+        <td>${row.payment}</td>
+        <td class="${row.statusColor}">${row.status}</td>
+        <td class="primary">Details</td>
+      </tr>
+    `;
   }
-  
 
   tbody.innerHTML = bodyContent;
 
   return tbody;
 };
 
+
 const buildUpdatesList = () => {
-  const updateData = UPDATE_DATA;
+  const updateData = nos;
 
   const div = document.createElement("div");
   div.classList.add("updates");
 
+  console.log(Object.values(dados))
   let updateContent = "";
-  for (const update of updateData) {
-    updateContent += `
+  for (const no of updateData) {
+    console.log(no);
+    conduct = Object.values(dados[no].Conduct)
+    for (const update of conduct) {
+      console.log(update);
+      updateContent += `
       <div class="update">
         <div class="profile-photo">
           <img src="${update.imgSrc}" />
         </div>
         <div class="message">
-          <p><b>${update.profileName}</b> ${update.message}</p>
-          <small class="text-muted">${update.updatedTime}</small>
+          <p><b>Leitura:</b>${update.leitura}</p>
+          <small class="text-muted">${update.hora}</small>
         </div>
       </div>
     `;
+    }
   }
 
   div.innerHTML = updateContent;
@@ -121,7 +124,7 @@ const buildUpdatesList = () => {
 };
 
 const buildSalesAnalytics = (element) => {
-  const salesAnalyticsData = salesAnalyticsData;
+  const salesAnalyticsData = SALES_ANALYTICS_DATA;
 
   for (const analytic of salesAnalyticsData) {
     const item = document.createElement("div");
@@ -171,5 +174,3 @@ themeToggler.addEventListener("click", () => {
   themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
   themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 });
-
-
